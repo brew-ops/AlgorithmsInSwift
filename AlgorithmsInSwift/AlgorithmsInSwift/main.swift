dfbc62d5f216aa6779b542a40569a13bf9ee9a82
Eric Wolfe <rojowolf21@gmail.com>
2017-11-18 13:22:35 -0600

    First commit

---
+++ AlgorithmsInSwift/AlgorithmsInSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AlgorithmsInSwift
+//
+//  Created by Eric Wolfe on 11/18/17.
+//  Copyright © 2017 BrewOps. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
